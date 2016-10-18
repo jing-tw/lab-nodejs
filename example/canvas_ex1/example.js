5f95b297a46780cfb09c57dd84a31f957bb5ae7c
tom_jing <tom_jing@compal.com>
2016-10-18 18:41:43 +0800

    add canvas example

---
+++ example/canvas_ex1/example.js
@@ -0,0 +1,31 @@
+var Canvas = require('canvas')
+  , Image = Canvas.Image
+  , canvas = new Canvas(200, 200)
+  , ctx = canvas.getContext('2d');
+
+ctx.font = '30px Impact';
+ctx.rotate(.1);
+ctx.fillText("Awesome!", 50, 100);
+
+var te = ctx.measureText('Awesome!');
+ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+ctx.beginPath();
+ctx.lineTo(50, 102);
+ctx.lineTo(50 + te.width, 102);
+ctx.stroke();
+
+
+var server,
+        port=2012,
+        http=require('http');
+        server=http.createServer(function(req,res){
+            res.writeHead(200,{'Content-type':'image/png'});
+            // res.write('<img src="' + canvas.toDataURL() + '" />');    // show the image
+            // res.end(canvas);
+            canvas.pngStream().pipe(res);
+            console.log('guest visted');
+        });
+
+server.listen(port);
+console.log('Server is running');
+
---
+++ example/canvas_ex1/local_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#sudo apt-get install libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++
+#npm install canvas
+node example.js
+#firefox example.html
