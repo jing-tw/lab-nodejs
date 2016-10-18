57284951b13d0cc3e7e7ad1e201f4c8b85586e52
tom_jing <tom_jing@compal.com>
2016-10-18 18:57:50 +0800

    add canvas rectangle example

---
+++ example/canvas_rectangle/example.js
@@ -0,0 +1,26 @@
+var Canvas = require('canvas')
+  , Image = Canvas.Image
+  , canvas = new Canvas(200, 200)
+  , ctx = canvas.getContext('2d');
+
+//define the fill colour of the rectangle
+ctx.fillStyle = "green";
+ctx.strokeStyle = "green";
+ctx.fillRect(1,10,100,30); 
+ctx.stroke();
+
+
+var server,
+        port=2012,
+        http=require('http');
+        server=http.createServer(function(req,res){
+            res.writeHead(200,{'Content-type':'image/png'});
+            // res.write('<img src="' + canvas.toDataURL() + '" />');    // show the image
+            // res.end(canvas);
+            canvas.pngStream().pipe(res);
+            console.log('guest visted');
+        });
+
+server.listen(port);
+console.log('Server is running');
+
