94f5e7d8801da31ab29c301bf6c797f906f086d4
tom_jing <tom_jing@compal.com>
2016-11-29 18:28:44 +0800

    [Add] file stat example

---
+++ example/file_stats/example.js
@@ -0,0 +1,11 @@
+let fs = require('fs');
+let util = require('util');
+
+function main(){
+  fs.stat('example.html', function (err, stats) {
+    if (err) return console.log(err);
+    console.log(util.inspect(stats));
+  });
+}
+
+main();
