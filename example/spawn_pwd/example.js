cc394fedbddfc97e17019d85fdb05ba080290f0d
tom_jing <tom_jing@compal.com>
2016-12-19 11:15:47 +0800

    [Add] How to pass argument to system command

--- example/spawn_pwd/example.js
+++ example/spawn_pwd/example.js
@@ -1,7 +1,9 @@
-var spawn = require('child_process').spawn,
-    pwd = spawn('pwd');
+var spawn = require('child_process').spawn
+
+var pwd = spawn('pwd');
 
 pwd.stdout.on('data', function (data) {
+  console.log(" == Normal Invoke == ");
   console.log('stdout: ' + data);
 });
 
@@ -11,4 +13,11 @@ pwd.stderr.on('data', function (data) {
 
 pwd.on('close', function (code) {
   console.log('child process exited with code ' + code);
+});
+
+var spawn2 = require('child_process').spawn
+var pwd_arg = spawn2('pwd', ['--version']);
+pwd_arg.stdout.on('data', function (data){
+  console.log(" == Pass Argument Demo == ");
+  console.log('pwd_arg stdout: ' + data);
 });
