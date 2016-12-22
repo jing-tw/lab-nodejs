5c8b4be1e2e721fda5ef04d7bab0e710b44ef718
tom_jing <tom_jing@compal.com>
2016-12-22 13:08:56 +0800

    [Add] sync file write and read

---
+++ example/generator_file_example/example.js
@@ -0,0 +1,46 @@
+var myGenerator;
+
+function main() {
+  myGenerator = MyGeneratorFunction();
+  var retObj = myGenerator.next();                // start to run the MyGeneratorFunction
+  console.log('myGenerator.next() = ', retObj);    
+
+  retObj = myGenerator.next();     // 
+  // console.log('myGenerator.next() = ', retObj); 
+
+}
+
+// Generator Function includes codes requiring to sync 
+function* MyGeneratorFunction(){   
+  yield doAsyncFn();              // Run doAsyncFn and then pause until next() was involded
+  yield doAyncFnThatDependsOnA(); // Run doAyncFnThatDependsOnA and then pause until next() was involded
+}
+
+// Async Function
+function doAsyncFn(){
+  let fs = require('fs');
+  console.log('run doAsyncFn()');
+
+  strCode = "Hellow World\n";
+  fs.writeFile("out.txt", strCode, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+      console.log("The file was saved!");
+      var retObj = myGenerator.next();     // <-------------- resume to the next
+      console.log('myGenerator.next() = ', retObj);
+   });
+}
+
+// Any Function that depends on Async Function
+function doAyncFnThatDependsOnA() {
+  let fs = require("fs");
+
+  var data = fs.readFileSync('out.txt');
+  console.log("Synchronous read: " + data.toString());
+  console.log("End");
+  return data;
+}
+
+main();
+
