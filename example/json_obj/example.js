5d2d2085e3ef9f4b4cc8b134da340df7377c00cb
tom_jing <tom_jing@compal.com>
2017-01-04 12:01:13 +0800

    [Fix] Fix restore issue

--- example/json_obj/example.js
+++ example/json_obj/example.js
@@ -14,16 +14,19 @@ class MyClass{
 }
 
 function main() {
-  // save to json
+  // Create objects
   let obj = new MyClass("Jing", "1234");
   console.log("Json" + obj.toJson());
   
   let obj2 = new MyClass("Tom", "57");
   console.log("Json" + obj2.toJson());
 
-  // restore
-  let restoreObj = new MyClass(JSON.parse(obj.toJson()));
-  restoreObj.print();
+
+  // Restore object
+  let restoreObj2 = JSON.parse(obj.toJson());
+  restoreObj2.__proto__ = MyClass.prototype;
+  console.log('restoreObj2 = ',  restoreObj2);
+  console.log('restoreObj2._strName = ' + restoreObj2._strName);
   
 }
 
