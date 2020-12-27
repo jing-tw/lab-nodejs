2439091b9046a7d3c2034551d8170f6552f706e9
jing-tw <mqjing@gmail.com>
2020-12-27 18:34:18 +0800

    [electron] electron + webpack, webapp

---
+++ example/desktop/electron/02-electron-webaudio-play-mp3/README.md
@@ -0,0 +1,14 @@
+# Install
+yarn install
+
+# Web app build
+yarn build
+
+# Web app test
+yarn test
+
+# Electron app build and run
+yarn electron-app:all
+
+Reference
+1. 
---
+++ example/desktop/electron/02-electron-webaudio-play-mp3/webpack.config.ts
@@ -0,0 +1,47 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config: webpack.Configuration = {
+	mode: 'production',
+	entry: './src/index.ts',   // <------- the entry file
+
+	// output bundle
+	output: {
+    	    path: path.resolve(__dirname, 'dist'),      // <------- bundles location
+    	    filename: 'main.bundle.js'     // <------------- setup bundle  file
+	},
+
+	// file resolutions
+	resolve: {
+    	    extensions: [ '.ts', '.js' ],
+    },
+
+	// ts-loader will enter through ./index.ts, load all .ts and .tsx files
+	module: {
+    	    rules: [
+        	        {
+            	            test: /\.tsx?/,
+            	            use: 'ts-loader',
+            	            exclude: /node_modules/,
+        	       }
+    	    ]
+	},
+
+	// for template index and copy specific file
+	plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+          }),
+    ],
+
+	// for webpack-dev-server
+	devtool: "source-map",  // <---- Enable sourcemaps for debugging webpack's output.
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 9000
+	}
+};
+
+export default config;
